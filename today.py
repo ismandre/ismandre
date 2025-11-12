f3998b26deb5bb56f8b06b9c2cd4a977c3412483
Marko Andreis <69961738+ismandre@users.noreply.github.com>
2025-11-12 23:01:15 +0100

    Update today.py

--- today.py
+++ today.py
@@ -2,4 +2,4 @@ from datetime import date
 
 with open("README.md", "w") as f:
   f.write(f"## Hello, today is {date.today()} ☀️\n")
-  f.write("![Header](./my_example.svg")
+  f.write("![Header](./my_example.svg)")
