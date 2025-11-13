53a8074cf5eb3412baf53e546f1ea4d49be90028
Marko Andreis <69961738+ismandre@users.noreply.github.com>
2025-11-13 16:37:26 +0100

    Update today.py

--- today.py
+++ today.py
@@ -1,5 +1,28 @@
+import xml.etree.ElementTree as ET
 from datetime import date
+from dateutil.relativedelta import relativedelta
 
-with open("README.md", "w") as f:
-  f.write(f"## Hello, today is {date.today()} ☀️\n")
-  f.write("![Header](./my_example.svg)")
+BIRTHDAY = date(year=1999, month=7, day=17)
+
+today = date.today()
+diff = relativedelta(today, BIRTHDAY)
+months_text = "months" if diff.months > 1 else "month"
+days_text = "days" if diff.days > 1 else "day"
+uptime_text = f"{diff.years} years, {diff.months} {months_text}, {diff.days} {days_text}"
+if diff.months == 0 and diff.days == 0:
+    uptime_text += " 🥳"
+
+input_file = "banner.svg"
+output_file = "banner.svg"
+
+ET.register_namespace("", "http://www.w3.org/2000/svg")
+
+tree = ET.parse(input_file)
+root = tree.getroot()
+
+namespaces = {"svg": "http://www.w3.org/2000/svg"}
+element = root.find(".//*[@id='age_data']", namespaces)
+
+if element is not None:
+    element.text = ": " + uptime_text
+    tree.write(output_file, encoding="utf-8", xml_declaration=True)
